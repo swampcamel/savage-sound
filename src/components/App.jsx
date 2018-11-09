53f3a07ed3242bfe8f386b40ff77de5ed30c03e7
Dan Mace <dmacebeta@gmail.com>
2018-11-09 10:15:56 -0800

    build dummy data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,68 @@ function App(){
   const mainStyles = {
     marginTop: "82px"
   }
+  const state = {
+    albumList: [
+      {
+        title: "Something Else",
+        artist: "The Brian Jonestown Massacre",
+        releaseDate: "May 25, 2018",
+        recordLabel: "A Records",
+        formats: ["CD", "LP", "DD"],
+        totalSongs: "9",
+        length: "39 min",
+        rating: "7",
+        starRating: "4",
+        trackList: [
+          {
+            songTitle: "Hold That Thought",
+            trackNumber: "1",
+            length: "4:47",
+            rating: "7"
+          },
+          {
+            songTitle: "Animal Wisdom",
+            trackNumber: "2",
+            length: "3:35",
+            rating: "8"
+          }
+        ]
+
+      },
+      {
+        title: "Wasteland",
+        artist: "Uncle Acid & The Deadbeats",
+        releaseDate: "October 12, 2018",
+        recordLabel: "Rise Above Records",
+        formats: ["CD", "LP"],
+        totalSongs: "8",
+        length: "46 min",
+        rating: "8",
+        starRating: "4.5",
+        trackList: [
+          {
+            songTitle: "I See Through You",
+            trackNumber: "1",
+            length: "5:42",
+            rating: "7"
+          },
+          {
+            songTitle: "Shockwave City",
+            trackNumber: "2",
+            length: "4:00",
+            rating: "8"
+          }
+        ]
+      }
+    ]
+  }
+  
   return (
     <div>
     <TopNav/>
-      <Switch>
-        <Route exact path='/' component={Home}/>
-      </Switch>
+    <Switch>
+    <Route exact path='/' component={Home}/>
+    </Switch>
     </div>
   );
 }
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
+
 import SearchBar from './SearchBar';
+import NavLinks from './NavLinks';
 
 function TopNav() {
   return (
     <div className="nav">
-    <style jsx>
-    {`
-      .nav {
-        height: 52px;
-        width: 100%;
-        border-bottom: 1px solid #e1e1e1;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-      }
-    `}
-    </style>
-    <div>
-    <h2>LOGO</h2>
-    </div>
-    <SearchBar/>
+      <style jsx>
+      {`
+        .nav {
+          height: 52px;
+          width: 100%;
+          border-bottom: 1px solid #e1e1e1;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+        }
+      `}
+      </style>
+      <div>
+      <h2>LOGO</h2>
+      </div>
+      <SearchBar/>
+      <NavLinks/>
     </div>
   )
 }
