652f9d89c296d2e38f5de3abca2a4cb72423ccf5
Dan Mace <dmacebeta@gmail.com>
2018-11-09 17:22:54 -0800

    add category component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Hero from "./Hero/Hero";
-
+import Category from "./Category";
 function Home() {
   return (
     <div>
-    <Hero/>
+      <Hero/>
+      <Category/>
     </div>
   )
 };
